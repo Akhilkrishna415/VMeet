46fe97d0ce3b0a2f0a7a0aeb0762806fed108662
Akhilkrishna415 <akhilkrishna185@gmail.com>
2020-09-17 14:44:12 -0400

    Added intents in Signup

--- app/src/main/java/com/example/vmeet/SignUp.java
+++ app/src/main/java/com/example/vmeet/SignUp.java
@@ -1,14 +1,36 @@
 package com.example.vmeet;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SignUp extends AppCompatActivity {
+    TextView userExist;
+    Button btnReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        userExist = findViewById(R.id.ExistingUser);
+        btnReg = findViewById(R.id.buttonRegister);
+        userExist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Login.class);
+                startActivity(i);
+            }
+        });
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Homepage.class);
+                startActivity(i);
+            }
+        });
     }
 }
