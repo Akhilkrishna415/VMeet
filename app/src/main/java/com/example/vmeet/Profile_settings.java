d0dcac3512373a8e10db5789f601557cf467259f
naveensadineni99 <naveensadineni7799@gmail.com>
2020-09-18 02:19:04 -0400

    created intents for settings page

---
+++ app/src/main/java/com/example/vmeet/Profile_settings.java
@@ -0,0 +1,14 @@
+package com.example.vmeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile_settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+    }
+}
---
+++ app/src/main/java/com/example/vmeet/Settings.java
@@ -0,0 +1,62 @@
+package com.example.vmeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Settings extends AppCompatActivity {
+
+    TextView tv_profilesettings;
+    TextView tv_contactus;
+    TextView tv_faqs;
+    TextView tv_feedback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        tv_profilesettings=findViewById(R.id.profile_settings);
+        tv_profilesettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),Profile_settings.class);
+                startActivity(i);
+
+            }
+        });
+        tv_faqs=findViewById(R.id.faqs);
+        tv_faqs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),Faqs.class);
+                startActivity(i);
+
+            }
+        });
+        tv_contactus=findViewById(R.id.contactus);
+        tv_contactus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),Contactus.class);
+                startActivity(i);
+
+            }
+        });
+        tv_feedback=findViewById(R.id.feedback);
+        tv_feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),Feedback.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
+    }
+}
