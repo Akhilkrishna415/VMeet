1615aa138e0bef6199a7ca58b6f2891aacb38b1d
SURYA123-bit <surya.pasumarthi111@gmail.com>
2020-09-23 22:30:41 -0400

    Added Splash Screen

--- app/src/main/java/com/example/vmeet/ResetActivity.java
+++ app/src/main/java/com/example/vmeet/ResetActivity.java
@@ -2,13 +2,28 @@ package com.example.vmeet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ResetActivity extends AppCompatActivity {
-
+    TextView Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset);
+        Login=findViewById(R.id.Login);
+
+
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Login.class);
+                startActivity(i);
+            }
+        });
     }
 }
