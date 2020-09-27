e99e5ad9f13fd56d252bd104009822b1b048e4f2
SURYA123-bit <surya.pasumarthi111@gmail.com>
2020-09-26 20:37:03 -0400

    Connected splash and fixed ui in login page

--- app/src/main/java/com/example/vmeet/SplashActivity.java
+++ app/src/main/java/com/example/vmeet/SplashActivity.java
@@ -2,13 +2,29 @@ package com.example.vmeet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
 
 public class SplashActivity extends AppCompatActivity {
-
+   Window win;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        win=this.getWindow();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent =  new Intent(SplashActivity.this, Login.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        },1800);
     }
 }
