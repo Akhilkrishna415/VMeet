a289b6701bec8a753b5f1c9e0307116a951ff434
nikgrover <nikhil.grover20@gmail.com>
2020-09-27 16:49:18 -0400

    java code for Adapter,Model(HomeFragment)

---
+++ app/src/main/java/com/example/vmeet/RecycleModel.java
@@ -0,0 +1,40 @@
+package com.example.vmeet;
+
+public class RecycleModel {
+
+    private String Title;
+    private String Room;
+    private String time;
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getRoom() {
+        return Room;
+    }
+
+    public void setRoom(String room) {
+        Room = room;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public RecycleModel(String title,String time, String room)
+    {
+        this.Title = title;
+        this.Room = room;
+        this.time = time;
+
+    }
+}
