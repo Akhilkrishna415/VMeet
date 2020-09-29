6ca86365d2cdf2127a2b09ebb90a85a37d94bb39
SURYA123-bit <surya.pasumarthi111@gmail.com>
2020-09-29 00:16:39 -0400

    Added circularimgview  in splash and createpostpage

--- app/src/main/java/com/example/vmeet/Homepage.java
+++ app/src/main/java/com/example/vmeet/Homepage.java
@@ -34,8 +34,8 @@ public class Homepage extends AppCompatActivity implements NavigationView.OnNavi
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(getApplicationContext(), CreatePostActivity.class);
+                startActivity(i);
             }
         });
         drawer = findViewById(R.id.drawer_layout);
