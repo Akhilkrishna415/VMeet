ebac2236e934694ca5476b76f7473f11a6cef5a5
Akhilkrishna415 <akhilkrishna185@gmail.com>
2020-09-29 11:58:57 -0400

    Firebase Auth for SignUp

--- app/src/main/java/com/example/vmeet/SignUp.java
+++ app/src/main/java/com/example/vmeet/SignUp.java
@@ -2,15 +2,37 @@ package com.example.vmeet;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class SignUp extends AppCompatActivity {
+    public static final String TAG = "TAG";
     TextView userExist;
     Button btnReg;
+    EditText etemail, etpwd, etphone, etstaffid, etdpart, etdesign;
+    FirebaseAuth mFirebaseAuth;
+    FirebaseFirestore fstore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +40,71 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         userExist = findViewById(R.id.ExistingUser);
         btnReg = findViewById(R.id.buttonRegister);
+        etemail = findViewById(R.id.editTextEmailAddress);
+        etpwd = findViewById(R.id.editTextPassword);
+        etphone = findViewById(R.id.editTextPhone);
+        etstaffid = findViewById(R.id.editTextStaffID);
+        etdpart = findViewById(R.id.editTextDepartment);
+        etdesign = findViewById(R.id.editTextDesignation);
+        fstore = FirebaseFirestore.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = etemail.getText().toString();
+                final String pwd = etpwd.getText().toString();
+                final String phone = etphone.getText().toString();
+                final String staffID = etstaffid.getText().toString();
+                final String depart = etdpart.getText().toString();
+                final String design = etdesign.getText().toString();
+                if (email.isEmpty()) {
+                    etemail.setError("Please enter the email ID");
+                    etemail.requestFocus();
+                } else if (pwd.isEmpty() && pwd.length() < 6) {
+                    etpwd.setError("Please enter the password with more than 6 characters");
+                    etpwd.requestFocus();
+                } else if (email.isEmpty() && pwd.isEmpty() && phone.isEmpty() && staffID.isEmpty() && depart.isEmpty() && design.isEmpty()) {
+                    Toast.makeText(SignUp.this, "Fields are Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!email.isEmpty() && !pwd.isEmpty()) {
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(SignUp.this, "SignUp Unsuccessful, PLease try again using another email!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(SignUp.this, "Registration Successful. You will be redirected to login page now.", Toast.LENGTH_SHORT).show();
+                                userID = mFirebaseAuth.getCurrentUser().getUid();
+                                DocumentReference documentReference = fstore.collection("Users").document(userID);
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("Email", email);
+                                user.put("Staffid", staffID);
+                                user.put("Phone Number", phone);
+                                user.put("Department", depart);
+                                user.put("Designation", design);
+                                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "onSuccess: user profile is created for" + userID);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error adding document", e);
+                                    }
+                                });
+                                startActivity(new Intent(SignUp.this, Login.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(SignUp.this, "Error Occurred!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         userExist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), Login.class);
-                startActivity(i);
-            }
-        });
-        btnReg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), Homepage.class);
                 startActivity(i);
             }
         });
