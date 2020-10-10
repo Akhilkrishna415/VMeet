ec28b0b10639ce55cb8dc4c1c05b008df23b2843
nikgrover <nikhil.grover20@gmail.com>
2020-10-10 15:14:26 -0400

    uploaded call / mail functionality v1

--- app/src/main/java/com/example/vmeet/Contactus.java
+++ app/src/main/java/com/example/vmeet/Contactus.java
@@ -5,8 +5,10 @@ import androidx.appcompat.widget.Toolbar;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Contactus extends AppCompatActivity {
 
@@ -38,5 +40,25 @@ public class Contactus extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void call(View view)
+    {
+        Intent call = new Intent(Intent.ACTION_DIAL);
+        call.setData(Uri.parse("tel:5135699966"));
+        startActivity(call);
+    }
+
+    public void Mail(View view)
+    {
+        Intent Mail = new Intent(Intent.ACTION_SEND);
+        String[] recipients={"mailto@gmail.com"};
+        Mail.putExtra(Intent.EXTRA_EMAIL, recipients);
+        Mail.putExtra(Intent.EXTRA_SUBJECT,"Subject text here...");
+        Mail.putExtra(Intent.EXTRA_TEXT,"Body of the content here...");
+        Mail.putExtra(Intent.EXTRA_CC,"secondcupcafemontreal@gmail.com");
+        Mail.setType("text/html");
+        Mail.setPackage("com.google.android.gm");
+        startActivity(Intent.createChooser(Mail, "Send mail"));
     }
 }
