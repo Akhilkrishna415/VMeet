4ab7cff5b60a54d71a69a7f80f3077540b54a0bb
nikgrover <nikhil.grover20@gmail.com>
2020-10-11 19:50:07 -0400

    tweakes

--- app/src/main/java/com/example/vmeet/MyBooking.java
+++ app/src/main/java/com/example/vmeet/MyBooking.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.vmeet.ui.home.HomeViewModel;
 
@@ -12,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyBooking extends AppCompatActivity {
-
 
     final List<RecycleModel> RecycleList = new ArrayList<>();
     String[] MeetingTitle = {"MAD315 exam", "CST 2020 Orientation","MAD415 Orientation","CST 2019 Workshop","Meeting with Student"};
--- app/src/main/java/com/example/vmeet/MyBookingAdapter.java
+++ app/src/main/java/com/example/vmeet/MyBookingAdapter.java
@@ -8,25 +8,26 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyBookingAdapter  extends RecyclerView.Adapter<MyBookingAdapter.ViewHolder>{
+public class MyBookingAdapter  extends RecyclerView.Adapter<MyBookingAdapter.ViewHolder> {
 
     String[] mTitle = {};
-    String [] mRoom ={};
+    String[] mRoom = {};
     String[] mTime = {};
 
     private LayoutInflater layoutInflater;
-    public MyBookingAdapter(String[] _data,String[] _data2,String[] _data3)
-    {
+
+    public MyBookingAdapter(String[] _data, String[] _data2, String[] _data3) {
         mTitle = _data;
         mTime = _data2;
         mRoom = _data3;
     }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         layoutInflater = LayoutInflater.from(parent.getContext());
 
-        View view = layoutInflater.inflate(R.layout.mybooking_model,parent,false);
+        View view = layoutInflater.inflate(R.layout.mybooking_model, parent, false);
         return new ViewHolder(view);
 
     }
@@ -49,16 +50,17 @@ public class MyBookingAdapter  extends RecyclerView.Adapter<MyBookingAdapter.Vie
         return mTitle.length;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder
-    {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         TextView Title;
         TextView Time;
         TextView Room;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             Title = itemView.findViewById(R.id.title);
             Time = itemView.findViewById(R.id.time);
             Room = itemView.findViewById(R.id.room);
         }
+
     }
 }
